8528101df151136ed5ba7bbb4a747c6f3c6cf820
unknown <matthewaustinbell@gmail.com>
2019-08-24 11:17:39 -0500

    wip

---
+++ Giraffe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+
+            Console.WriteLine( phrase.Substring() );
+
+            Console.ReadLine();
+        }
+    }
+}
